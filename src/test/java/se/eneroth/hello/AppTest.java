fa46bcd14f40f5f83491dbea378f38ec7e40cf7c
Rickard <rickard_eneroth@hotmail.com>
2014-12-10 20:15:04 +0100

    New

--- src/test/java/se/eneroth/hello/AppTest.java
+++ src/test/java/se/eneroth/hello/AppTest.java
@@ -1,38 +1,18 @@
 package se.eneroth.hello;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    @Test
+    public void testa() {
+        System.out.println("Testar...");
+        Assert.assertEquals(7,7);
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
     }
 }
