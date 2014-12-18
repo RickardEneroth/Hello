533be4e9e10301be606a68d33423e8f5f4462f17
Rickard <rickard_eneroth@hotmail.com>
2014-12-18 19:51:04 +0100

    add

--- src/main/java/se/eneroth/hello/App.java
+++ src/main/java/se/eneroth/hello/App.java
@@ -4,4 +4,8 @@ public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
+
+    public int add(int tal1, int tal2) {
+        return tal1 + tal2;
+    }
 }
--- src/test/java/se/eneroth/hello/AppTest.java
+++ src/test/java/se/eneroth/hello/AppTest.java
@@ -1,13 +1,20 @@
 package se.eneroth.hello;
 
 import org.testng.annotations.Test;
-import org.testng.Assert;
+import static org.testng.Assert.*;
 
 public class AppTest {
 
     @Test
     public void testa() {
         System.out.println("Testar...");
-        Assert.assertEquals(7, 7);
+        assertEquals(7, 7);
+    }
+
+    @Test
+    public void test_add() {
+        App app = new App();
+        int result = app.add(7, 8);
+        assertEquals(result, 15);
     }
 }
