b07969cc537ffdfd46040cf2369e72c2139f6c03
Rickard <rickard_eneroth@hotmail.com>
2014-12-18 20:19:01 +0100

    array2

--- src/main/java/se/eneroth/hello/App.java
+++ src/main/java/se/eneroth/hello/App.java
@@ -1,8 +1,14 @@
 package se.eneroth.hello;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        new App().array();
+        new App().array2();
     }
 
     public int add(int tal1, int tal2) {
@@ -12,5 +18,30 @@ public class App {
     // To upper case
     public String up(String str) {
         return str.toUpperCase();
+    }
+
+    /* Array */
+    public void array() {
+        List<String> list = new ArrayList<String>();
+        list.add("apa");
+        list.add("fisk");
+        list.add("get");
+
+        Iterator i = list.iterator();
+        while (i.hasNext()) {
+            System.out.println(i.next());
+        }
+    }
+
+    /* Array2 */
+    public void array2() {
+        List<String> list = new ArrayList<String>();
+        list.add("apa");
+        list.add("fisk");
+        list.add("get");
+
+        for (Object aList : list) {
+            System.out.println(aList);
+        }
     }
  }
