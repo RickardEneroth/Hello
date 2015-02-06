b9ce91377fd78d19f057b2a730ac6ef136575271
Rickard <rickard_eneroth@hotmail.com>
2015-02-06 17:55:17 +0100

    Closures

--- src/main/java/se/eneroth/hello/Java8Closures.java
+++ src/main/java/se/eneroth/hello/Java8Closures.java
@@ -67,8 +67,7 @@ public class Java8Closures {
         );
     }
 
-    public static void printPersons(
-            List<Person> roster, CheckPerson tester) {
+    public static void printPersons(List<Person> roster, CheckPerson tester) {
         for (Person p : roster) {
             if (tester.test(p)) {
                 p.printPerson();
